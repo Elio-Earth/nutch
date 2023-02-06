e74e2ee1e35d9cef79c4efa526d13dd9a0219aea
Kamil Mroczek <kamil.mroczek@gmail.com>
2023-02-06 17:09:41 -0500

    JSONIndexer: Allow forcing some fields in indexing to be single fields.
    - Configure which fields we want force to be single valued. Some pages we encountered have duplicated meta tags on the page and this was leading to some values being arrays while others were non-arrays (this is a schema breaking issue)
    - Updated path for JSON to use partition name "insert_date" vs "insert" since insert is a reserved word for SQL.

--- src/plugin/indexer-json/src/java/earth/elio/nutch/indexwriter/json/JsonConstants.java
+++ src/plugin/indexer-json/src/java/earth/elio/nutch/indexwriter/json/JsonConstants.java
@@ -4,6 +4,8 @@ public interface JsonConstants {
 
     String JSON_FIELDS = "fields";
 
+    String FORCE_SINGLE_FIELDS = "force_single";
+
     String JSON_BASE_OUTPUT_PATH = "base_output_path";
 
 }
